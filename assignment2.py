92919e16ae788be6890b0f6d5695d54721ab2235
Danil Vorotilov <danilvorotilov@tudelft.nl>
2026-05-08 11:00:04 +0200

    work in assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -28,18 +28,31 @@ SERVER_PUB_KEY = "4c69624e61434c504b3a82e33614a342774e084af80835838d6dbdb64a537d
 SERVER_PUB_KEY_SHA1 = hashlib.sha1(bytes.fromhex(SERVER_PUB_KEY))
 COMMUNITY_ID = "4c61623247726f75705369676e696e6732303236"
 REPLICATION_COMMUNITY_ID = "0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF"
-DIFFICULTY = bytes.fromhex("0000000f" + "f" * 56)
 
-MEMBER_KEYS = {
+
+PUBLIC_KEYS = {
     "1": "4c69624e61434c504b3ab61e37a8692d6daa52ffadd042aa7b271121397fb2183b0690e3f29eb70d535ab3a5844182e0193bbda94b4007be5cc0f96aeadd130c6e48542c0109e5f18803",
     "2": "4c69624e61434c504b3acb4cf8cd94d4c0b6513dde5ac3e713421243fe03acd9f81c44a3c59d665af57e9372a84599691d8ca03efbe0095cc5eb4a14d68700ab81356a4da03be942c848",
     "3": "4c69624e61434c504b3af9e8ecfcb5968c5438c65adf621afcb336895329da741ef0e1ff846db37f3a1dd4188afcad7d8f8a890571930a4bb7b982904911437c2aba97922746c5fdb176"
 }
 
-LASTSENT = "_lastsent"
+MEMBER_KEYS = {
+    "4c69624e61434c504b3ab61e37a8692d6daa52ffadd042aa7b271121397fb2183b0690e3f29eb70d535ab3a5844182e0193bbda94b4007be5cc0f96aeadd130c6e48542c0109e5f18803": "1",
+    "4c69624e61434c504b3acb4cf8cd94d4c0b6513dde5ac3e713421243fe03acd9f81c44a3c59d665af57e9372a84599691d8ca03efbe0095cc5eb4a14d68700ab81356a4da03be942c848": "2",
+    "4c69624e61434c504b3af9e8ecfcb5968c5438c65adf621afcb336895329da741ef0e1ff846db37f3a1dd4188afcad7d8f8a890571930a4bb7b982904911437c2aba97922746c5fdb176": "3"
+}
 
+LASTSENT = "_lastsent"
 IS_LEADER = True
 
+# Global variables
+global to_send
+global group_id
+global solution_dict
+global server_peer
+solution_dict = dict()
+global round_nr
+round_nr = 1
 
 
 
@@ -91,6 +104,8 @@ class SubmissionCommunity(Community, PeerObserver):
         self.add_message_handler(RegistrationResponse, self.on_registration_response)
         self.add_message_handler(ChallangeResponse, self.on_challange_response)
         self.add_message_handler(RoundResult, self.on_round_result)
+        self.register_task("check_solutions", self.check_solutions, interval = 0.1)
+    
 
 
 
@@ -98,41 +113,88 @@ class SubmissionCommunity(Community, PeerObserver):
     def started(self) -> None:
         print("joining community")
         print("starting a peer listener")
+        print("my key", self.my_peer.public_key.key_to_bin().hex())
+
         self.network.add_peer_observer(self)
 
-        print("my key", self.my_peer.public_key.key_to_bin().hex())
+        
         
         
 
     # Callbacks
+    def check_solutions(self):
+        global group_id, solution_dict, round_nr, to_send, server_peer
+        ids = []
+        sum = 0
+        for key in list(solution_dict.keys()):
+            args = key.split("_")
+            if len(args) < 2:
+                continue
+            # TODO: Make sure temp_id is indeed in range [1..3]
+            temp_id = args[0]
+            curr_round_nr = args[1]
+            
+            if curr_round_nr == round_nr and temp_id not in ids:
+                ids.append(temp_id)
+                sum+=1
+        
+        if sum != 3:
+            return
+        # We know these exist
+        sig1 = solution_dict["1_"+round_nr]
+        sig2 = solution_dict["2_"+round_nr]
+        sig3 = solution_dict["3_"+round_nr]
+        # here sum is 3, so we are ready to send the packaged solution
+        to_send = BundleSubmission(
+            group_id,
+            round_nr,
+            sig1,
+            sig2,
+            sig3
+        )
+
+        if not server_peer:
+            print("[WARNING] server was never found as a peer")
+
+        server_peer.ez_send(to_send)
+
+
+
+
+
+
     def on_peer_added(self, peer):
+        global server_peer
         print(f"FOUND PEER: {peer}")
         print(f"-> mid: {peer.mid.hex()}")
         print(f"-> pkeybin: {peer.public_key.key_to_bin().hex()}")
 
         if peer.mid == SERVER_PUB_KEY_SHA1 or peer.public_key.key_to_bin() == bytes.fromhex(SERVER_PUB_KEY):
             print("FOUND SERVER, SENDING REGISTRATION REQUEST")
+            server_peer = peer
             self.ez_send(peer, RegistrationRequest(
-                MEMBER_KEYS["1"], 
-                MEMBER_KEYS["2"], 
-                MEMBER_KEYS["3"]
-            ))
-    
+                bytes.fromhex(PUBLIC_KEYS["1"]), 
+                bytes.fromhex(PUBLIC_KEYS["2"]), 
+                bytes.fromhex(PUBLIC_KEYS["3"])
+            ))    
 
     def on_peer_removed(self, peer) -> None:
         print(f"peer {peer} left")
 
     @lazy_wrapper(RegistrationResponse)
     def on_registration_response(self, peer, payload:RegistrationResponse):
+        global group_id
         print("success", payload.success)
         print("msg", payload.message)
+        group_id = payload.group_id
 
-    # TODO: implement callbacks
     @lazy_wrapper(ChallangeResponse)
     def on_challange_response(self, peer, payload:ChallangeResponse):
-        # Naive approach, just send the signed answer
-        signed_payload = default_eccrypto.create_signature(cast("PrivateKey", self.my_peer.key), payload)
-        peer.ez_send(signed_payload)
+       
+        global round_nr, solution_dict
+        my_submition_id = MEMBER_KEYS[self.my_peer.mid]
+        signed_nonce = default_eccrypto.create_signature(cast("PrivateKey", self.my_peer.key), payload.nonce).hex()
+        solution_dict[my_submition_id + "_" + round_nr] = signed_nonce
 
     def on_round_result(self, peer):
         pass
@@ -146,10 +208,12 @@ class Ping(VariablePayload):
     format_list = ["d"]
     names = ["timestamp"]
 
+
 class Solution(VariablePayload):
     msg_id=11
-    format_list = [""]
-    names = ["pkey"]
+    format_list = ["varlenH", "q"]
+    names = ["signed_nonce", "round_nr"]
+
 
 class PingCache(RandomNumberCacheWithName):
 
@@ -172,6 +236,7 @@ class ReplicationCommunity(Community):
         # Register the handler for the server's response
         
         self.add_message_handler(Ping, self.on_ping)
+        self.add_message_handler(Ping, self.on_solution)
 
         self.request_cache = RequestCache()
 
@@ -186,6 +251,15 @@ class ReplicationCommunity(Community):
         self.request_cache.add(PingCache(self.request_cache, peer.mid.hex() + LASTSENT, time.time()))
         time.sleep(rand_in_range(self.ping_delay_min, self.ping_delay_max))
         peer.ez_send(Ping(time.time()))
+
+    @lazy_wrapper(Solution)
+    def on_solution(self, peer, payload:Solution):
+        global solution_dict
+        if not payload.signed_nonce:
+            return
+        # add a key:value pair of the members registered num and round as key [1..3] , and their signed_nonce as value.
+        solution_dict[MEMBER_KEYS[peer.mid] + "_" + payload.round_nr] = payload.signed_nonce
+        
 
     # after timeout remove peer
     def on_ping_timeout(self, peer):
